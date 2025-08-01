ce0e6617fb39d3eaf7098213fea5754922c10d78
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-08-01 23:16:39 +0530

    Add sinhala song

--- LyricLine.java
+++ LyricLine.java
@@ -1,11 +1,11 @@
 public class LyricLine {
-        String text;
-        int durationInSeconds;
-        String colorCode;
+    String text;
+    int durationInSeconds;
+    String colorCode;
 
-        public LyricLine(String text, int durationInSeconds, String colorCode) {
-            this.text = text;
-            this.durationInSeconds = durationInSeconds;
-            this.colorCode = colorCode;
-        }
+    public LyricLine(String text, int durationInSeconds, String colorCode) {
+        this.text = text;
+        this.durationInSeconds = durationInSeconds;
+        this.colorCode = colorCode;
     }
+}
--- SongLyricsPlayer.java
+++ SongLyricsPlayer.java
@@ -4,13 +4,27 @@ public class SongLyricsPlayer {
 
     public static void main(String[] args) {
         List<LyricLine> lyrics = Arrays.asList(
-                new LyricLine("Hello, it's me", 2, AnsiColors.RED),
-                new LyricLine("I was wondering if after all these years", 4, AnsiColors.GREEN),
-                new LyricLine("You'd like to meet", 3, AnsiColors.YELLOW));
+                new LyricLine("\nතාරුකා නිවා", 2, AnsiColors.RED),
+                new LyricLine("දුර ඈත තනි වෙලා", 3, AnsiColors.RED),
+                new LyricLine("කොහේ ගියාද දිව්\u200Dයාංගනා", 4, AnsiColors.RED),
+
+                new LyricLine("             ", 2, AnsiColors.RED),
+
+                new LyricLine("ජීවිතේ කියා", 2, AnsiColors.GREEN),
+                new LyricLine("පෙර දාක රැවටිලා", 3, AnsiColors.GREEN),
+                new LyricLine("මගේ නොවේද දිව්\u200Dයාංගනා", 4, AnsiColors.GREEN),
+
+                new LyricLine("             ", 2, AnsiColors.RED),
+
+                new LyricLine("නික්මී නොයා", 2, AnsiColors.BLUE),
+                new LyricLine("මහදේ දොර වසා", 3, AnsiColors.BLUE),
+                new LyricLine("හදේ රැඳෙන්න දිව්\u200Dයාංගනා", 4, AnsiColors.BLUE),
+                new LyricLine("ළං වී ඉඳින්න දිව්\u200Dයාංගනා", 4, AnsiColors.BLUE)
+        );
 
         for (LyricLine line : lyrics) {
             printTypingEffectWithDuration(line.text, line.colorCode, line.durationInSeconds * 1000);
-            System.out.println(); 
+            System.out.println();
         }
     }
 
